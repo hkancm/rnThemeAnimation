742551499aca35282dab3dda948dee1df9eea70f
Hakan ÇAM <hkancm@gmail.com>
2024-03-17 13:37:21 +0300

    ThemeAnimation

--- App.js
+++ App.js
@@ -1,21 +1,118 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Switch, Dimensions, StatusBar } from "react-native";
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useDerivedValue,
+  withSpring,
+} from "react-native-reanimated";
+
+const SIZE = Dimensions.get("window").width * 0.7;
+
+const Colors = {
+  dark: {
+    background: "#1E1E1E",
+    circle: "#252525",
+    text: "#F8F8F8",
+  },
+  light: {
+    background: "#F8F8F8",
+    circle: "#FFF",
+    text: "#1E1E1E",
+  },
+};
+
+const SWITCH_TRACK_COLOR = {
+  true: "rgba(256,0,256,0.2)",
+  false: "rgba(0,0,0,0.1)",
+};
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  const progress = useDerivedValue(() => {
+    return theme === "dark"
+      ? withSpring(1, { stiffness: 15 })
+      : withSpring(0, { stiffness: 15 });
+  }, [theme]);
+
+  const rStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.background, Colors.dark.background],
+    );
+    return { backgroundColor };
+  });
+
+  const rCircleStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.circle, Colors.dark.circle],
+    );
+    return { backgroundColor };
+  });
+
+  const rTextStyle = useAnimatedStyle(() => {
+    const color = interpolateColor(
+      progress.value,
+      [0, 1],
+      [Colors.light.text, Colors.dark.text],
+    );
+    return { color };
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Animated.View style={[styles.container, rStyle]}>
+      <StatusBar
+        barStyle={theme === "light" ? "dark-content" : "light-content"}
+        backgroundColor={theme === "light" ? "#F8F8F8" : "#1E1E1E"}
+      />
+      <Animated.Text style={[styles.text, rTextStyle]}>
+        {theme.toUpperCase()}
+      </Animated.Text>
+      <Animated.View style={[styles.circle, rCircleStyle]}>
+        <Switch
+          value={theme === "dark"}
+          onValueChange={(toggled) => {
+            setTheme(toggled ? "dark" : "light");
+          }}
+          trackColor={SWITCH_TRACK_COLOR}
+          thumbColor={"rgba(212,22,191,.8)"}
+        />
+      </Animated.View>
+    </Animated.View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  circle: {
+    height: SIZE,
+    width: SIZE,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: SIZE / 2,
+    shadowOffset: {
+      width: 0,
+      height: 20,
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+    backgroundColor: "#FFF",
+    elevation: 8,
+    shadowColor: "rgba(255,50,50,.5)",
+  },
+  text: {
+    fontSize: 70,
+    textTransform: "uppercase",
+    fontWeight: "700",
+    letterSpacing: 14,
+    marginBottom: 35,
   },
 });
